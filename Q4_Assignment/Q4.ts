e5d8f9cc75aed891228f6f548c56f85130a16383
Ali Raza <159871669+AliRaza4112@users.noreply.github.com>
2024-03-01 18:11:32 +0500

    Add files via upload

---
+++ Q4_Assignment/Q4.ts
@@ -0,0 +1,7 @@
+// 4. Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the
+// following, including the quotation marks:
+
+// Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+    let name1:string="Allama Iqbal Said"
+     console.log(`${name1}, “Nations are born in the hearts of poets, they prosper and die in the hands of politicians.”`)
