028a2a337e976cbbbbeb260f0b422fc1043fa0d9
Ali Raza <159871669+AliRaza4112@users.noreply.github.com>
2024-03-10 12:18:17 +0500

    Add files via upload

---
+++ Q14/Q14.ts
@@ -0,0 +1,13 @@
+/* If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+*/
+
+
+const myFriends: string [] = ["Yasir","Muzamil","Ali","Ahmed"]
+
+    console.log(`Mr. ${myFriends[0]}! you are invited to my dinner on Saturday \n`);
+    console.log(`Mr. ${myFriends[1]}! you are invited to my dinner on Saturday \n`);
+    console.log(`Mr. ${myFriends[2]}! you are invited to my dinner on Saturday \n`);
+    console.log(`Mr. ${myFriends[3]}! you are invited to my dinner on Saturday \n`);
+    
+
+
